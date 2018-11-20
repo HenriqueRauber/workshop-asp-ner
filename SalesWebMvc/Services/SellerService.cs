45127b68016984cf272a0f7a87f52751371ba56b
Henrique Rauber <henriquerauber@gmail.com>
2018-11-20 12:45:44 -0200

    Seller detail and eanger loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -28,7 +29,11 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            //Isto retorna apenas o Seller:
+            //return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+
+            //Isto retorna o Seller com um join em department
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
